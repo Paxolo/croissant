21b0f7ae33a2d71c5024cb1c706ee4729e9504da
Stefano <53195387+Paxolo@users.noreply.github.com>
2023-04-24 17:21:12 +0200

    Update Emoji.java

---
+++ src/main/java/me/stefano/croissant/message/Emoji.java
@@ -0,0 +1,20 @@
+package me.stefano.croissant.message;
+
+public interface Emoji {
+
+    String HEART = "❤️";
+    String SWAG = "😎";
+    String PARTY = "🥳";
+    String ARROW_LEFT = "⬅️";
+    String ARROW_RIGHT = "➡️";
+    String ARROW_UP = "⬆️";
+    String ARROW_DOWN = "⬇️";
+    String TOP = "🔝";
+    String CONFIRM = "✅";
+    String CANCEL = "❌";
+    String EXCLAMATION_RED = "❗️";
+    String EXCLAMATION_WHITE = "❕";
+    String QUESTION_RED = "❓";
+    String QUESTION_WHITE = "❔";
+
+}
--- src/main/java/me/stefano/croissant/util/Emoji.java
+++
@@ -1,7 +0,0 @@
-package me.stefano.croissant.util;
-
-public interface Emoji {
-
-
-
-}
